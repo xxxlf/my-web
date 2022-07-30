095ac9d772d3e76b80ad44a47e94b4221f8857f5
xxxxxxlf <157807450@qq.com>
2022-07-31 02:20:01 +0800

    config:prod webpack config output.publicPach change

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,7 +4,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
   output: {
     filename: "script/[name].[contenthash].bundel.js",
-    publicPath: "/"
+    publicPath: "https://xxxlf.github.io/"
   },
   mode: "production",
   performance: {
